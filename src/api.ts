d6a1972bd735fde069f48102a9525d417e51c7fe
Melissa Hale <melissakayh@gmail.com>
2023-07-02 16:08:26 -0500

    new endpoints n stuff

--- src/api.ts
+++ src/api.ts
@@ -11,14 +11,21 @@ app.use(express.text({ type: 'text/html' }));
 
 // Healthcheck endpoint
 app.get('/', (req, res) => {
+  console.log(process.env)
   res.status(200).send({ status: 'ok' });
 });
 
 const api = express.Router();
 
 api.get('/hello', (req, res) => {
+  console.log(process.env)
   res.status(200).send({ message: 'hello world' });
 });
 
+api.get('/creds', (req, res) => {
+  console.log(process.env)
+  res.status(200).send({ message: {pg_user: process.env.PGUSER, pg_pass: process.env.PGPASS} });
+});
+
 // Version the api
-app.use('/api/v1', api);
+app.use(`/${process.env.API_ROOT}/v1`, api);
